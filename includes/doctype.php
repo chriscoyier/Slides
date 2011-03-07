091e415bde58047174c8c2f9d36e437f0d5f693e
Chris Coyier <chriscoyier@gmail.com>
2011-03-06 21:19:24 -0500

    strip tags from slide title when used for actual page title

--- includes/doctype.php
+++ includes/doctype.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	
-	<title><?php echo $title; ?></title>
+	<title><?php echo strip_tags($title); ?></title>
 	
 	<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
