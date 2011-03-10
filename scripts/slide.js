c45b0362ee7b619e23f7b294114402742d2ba9a9
Chris Coyier <chriscoyier@gmail.com>
2011-03-09 21:38:28 -0500

    keyboard navigation (left arrow goes back slide, right arrow goes forward slide).
    What's cool? Kensington Presenter keys map to those same keys so you aren't chained to computer giving presentation.

--- scripts/slide.js
+++ scripts/slide.js
@@ -18,3 +18,14 @@ $("#jumper").change(function() {
  	$("html").addClass("leaving");
 	setTimeout(gotoThere, 400, $(this).val());
 });
+
+$(document).keydown(function(e) {
+	// right arrow
+	if (e.keyCode == 39) {
+		$("#next").click();
+	}
+	// left arrow
+	if (e.keyCode == 37) {
+		$("#previous").click();
+	}
+});
