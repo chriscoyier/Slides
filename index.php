d6cb217556de78f5ba91343ba2198e98f9227d04
Chris Coyier <chriscoyier@gmail.com>
2011-03-31 12:52:41 -0400

    intro page

--- index.php
+++ index.php
@@ -3,18 +3,36 @@
 
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/splash.css">
 	<title>Slides</title>
 </head>
 
 <body>
 
-	<h1>Slides</h1>
-
-	<h2>A PHP &amp; HTML5 &amp; CSS3 &amp; jQuery Framework for creating a simple slide deck</h2>
-
-	<p>Going to be some kind of introductory page here</p>
+	<h1>
+		<span>S</span>
+		<span>l</span>
+		<span>i</span>
+		<span>d</span>
+		<span>e</span>
+		<span>s</span>
+		<div></div>
+	</h1>
 	
-	<p><a href="slides/1-intro.php">Go to actual slide deck</a></p>
+	<section id="about">
+
+		<h2>A <span>PHP</span> &amp; <span>HTML5</span> &amp; <span>CSS3</span> &amp; <span>jQuery</span> "framework" for creating a simple in-browser slide deck</h2>
+
+		<ol>
+			<li>Put this somewhere PHP can run.</li>
+			<li>Create slides in /slides/ folder, using templates already there.</li>
+			<li>Alter <code>setup.php</code> to name your deck and slides.</li>
+			<li>Present!</li>
+		</ol>
+	
+		<p><a href="slides/1-intro.php">Let's Rock &rarr;</a></p>
+	
+	</section>
 
 </body>
 
