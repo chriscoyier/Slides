f3a9309bda5cca3cc0b147a3b4aa52a9794488bc
Chris Coyier <chriscoyier@gmail.com>
2011-03-31 12:52:59 -0400

    prevent window load event from happening too fast

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,29 +1,44 @@
-$(window).load(function() {
-	$("html").addClass("loaded");
-});
+$(function() {
 
-function gotoThere(link) {
-	window.location = link;
-}
+	// Prevent window.load from happening too fast
+	// Idea by Chris Coyier and debungled by Paul Irish
+	(function fn() {
+      fn.now = +new Date;
 
-$("#previous, #next").click(function(e) {
-	$("html").addClass("leaving");
-	setTimeout(gotoThere, 1500, this.href);
-	return false;
-});
+      $(window).load(function() {
+	     // 500ms seems to be enough... feel free to adjust per machine
+         if (+new Date - fn.now < 500) setTimeout(fn, 500);
+         $("html").addClass("loaded");
+      });
 
-$("#jumper").change(function() {
- 	$("html").addClass("leaving");
-	setTimeout(gotoThere, 1500, $(this).val());
-});
+    })();
 
-$(document).keydown(function(e) {
-	// right arrow
-	if (e.keyCode == 39) {
-		$("#next").click();
+	// Simple redirect function
+	function gotoThere(link) {
+		window.location = link;
 	}
-	// left arrow
-	if (e.keyCode == 37) {
-		$("#previous").click();
-	}
+
+	$("#previous, #next").click(function(e) {
+		$("html").addClass("leaving");
+		setTimeout(gotoThere, 1500, this.href);
+		return false;
+	});
+
+	$("#jumper").change(function() {
+	 	$("html").addClass("leaving");
+		setTimeout(gotoThere, 1500, $(this).val());
+	});
+
+	// Left and right arrow keys for changing slides
+	$(document).keydown(function(e) {
+		// right arrow
+		if (e.keyCode == 39) {
+			gotoThere($("#next").attr("href"));
+		}
+		// left arrow
+		if (e.keyCode == 37) {
+			gotoThere($("#previous").attr("href"));
+		}
+	});
+
 });
